e5358bf660c785a91cfc4ff752d78488107a4301
Dirkjan Ochtman <dirkjan@ochtman.nl>
2019-08-01 12:09:33 +0200

    Replace doc comments with normal comments

--- imap-proto/src/core.rs
+++ imap-proto/src/core.rs
@@ -2,22 +2,22 @@ use nom::{self, IResult};
 
 use std::str;
 
-/// list-wildcards = "%" / "*"
+// list-wildcards = "%" / "*"
 pub fn list_wildcards(c: u8) -> bool {
     c == b'%' || c == b'*'
 }
 
-/// quoted-specials = DQUOTE / "\"
+// quoted-specials = DQUOTE / "\"
 pub fn quoted_specials(c: u8) -> bool {
     c == b'"' || c == b'\\'
 }
 
-/// resp-specials = "]"
+// resp-specials = "]"
 pub fn resp_specials(c: u8) -> bool {
     c == b']'
 }
 
-/// atom-specials = "(" / ")" / "{" / SP / CTL / list-wildcards / quoted-specials / resp-specials
+// atom-specials = "(" / ")" / "{" / SP / CTL / list-wildcards / quoted-specials / resp-specials
 pub fn atom_specials(c: u8) -> bool {
     c == b'('
         || c == b')'
@@ -29,20 +29,20 @@ pub fn atom_specials(c: u8) -> bool {
         || resp_specials(c)
 }
 
-/// ATOM-CHAR = <any CHAR except atom-specials>
+// ATOM-CHAR = <any CHAR except atom-specials>
 pub fn atom_char(c: u8) -> bool {
     !atom_specials(c)
 }
 
-/// nil = "NIL"
+// nil = "NIL"
 named!(pub nil, tag_s!("NIL"));
 
-/// ASTRING-CHAR = ATOM-CHAR / resp-specials
+// ASTRING-CHAR = ATOM-CHAR / resp-specials
 pub fn astring_char(c: u8) -> bool {
     atom_char(c) || resp_specials(c)
 }
 
-/// QUOTED-CHAR = <any TEXT-CHAR except quoted-specials> / "\" quoted-specials
+// QUOTED-CHAR = <any TEXT-CHAR except quoted-specials> / "\" quoted-specials
 pub fn quoted_data(i: &[u8]) -> IResult<&[u8], &[u8]> {
     // Ideally this should use nom's `escaped` macro, but it suffers from broken
     // type inference unless compiled with the verbose-errors feature enabled.
@@ -62,18 +62,18 @@ pub fn quoted_data(i: &[u8]) -> IResult<&[u8], &[u8]> {
     Ok((&i[len..], &i[..len]))
 }
 
-/// quoted = DQUOTE *QUOTED-CHAR DQUOTE
+// quoted = DQUOTE *QUOTED-CHAR DQUOTE
 named!(pub quoted<&[u8]>, delimited!(
     char!('"'),
     quoted_data,
     char!('"')
 ));
 
-/// quoted bytes as as utf8
+// quoted bytes as as utf8
 named!(pub quoted_utf8<&str>, map_res!(quoted, str::from_utf8));
 
-/// literal = "{" number "}" CRLF *CHAR8
-///            ; Number represents the number of CHAR8s
+// literal = "{" number "}" CRLF *CHAR8
+//            ; Number represents the number of CHAR8s
 named!(pub literal<&[u8]>, do_parse!(
     tag_s!("{") >>
     len: number >>
@@ -83,52 +83,52 @@ named!(pub literal<&[u8]>, do_parse!(
     (data)
 ));
 
-/// string = quoted / literal
+// string = quoted / literal
 named!(pub string<&[u8]>, alt!(quoted | literal));
 
-/// string bytes as as utf8
+// string bytes as as utf8
 named!(pub string_utf8<&str>, map_res!(string, str::from_utf8));
 
-/// nstring = string / nil
+// nstring = string / nil
 named!(pub nstring<Option<&[u8]>>, alt!(
     map!(nil, |_| None) |
     map!(string, |s| Some(s))
 ));
 
-/// nstring bytes as utf8
+// nstring bytes as utf8
 named!(pub nstring_utf8<Option<&str>>, alt!(
     map!(nil, |_| None) |
     map!(string_utf8, |s| Some(s))
 ));
 
-/// number          = 1*DIGIT
-///                    ; Unsigned 32-bit integer
-///                    ; (0 <= n < 4,294,967,296)
+// number          = 1*DIGIT
+//                    ; Unsigned 32-bit integer
+//                    ; (0 <= n < 4,294,967,296)
 named!(pub number<u32>, flat_map!(nom::digit, parse_to!(u32)));
 
-/// same as `number` but 64-bit
+// same as `number` but 64-bit
 named!(pub number_64<u64>, flat_map!(nom::digit, parse_to!(u64)));
 
-/// atom = 1*ATOM-CHAR
+// atom = 1*ATOM-CHAR
 named!(pub atom<&str>, map_res!(take_while1_s!(atom_char),
     str::from_utf8
 ));
 
-/// astring = 1*ASTRING-CHAR / string
+// astring = 1*ASTRING-CHAR / string
 named!(pub astring<&[u8]>, alt!(
     take_while1!(astring_char) |
     string
 ));
 
-/// astring bytes as as utf8
+// astring bytes as as utf8
 named!(pub astring_utf8<&str>, map_res!(astring, str::from_utf8));
 
-/// text = 1*TEXT-CHAR
+// text = 1*TEXT-CHAR
 named!(pub text<&str>, map_res!(take_while_s!(text_char),
     str::from_utf8
 ));
 
-/// TEXT-CHAR = <any CHAR except CR and LF>
+// TEXT-CHAR = <any CHAR except CR and LF>
 pub fn text_char(c: u8) -> bool {
     c != b'\r' && c != b'\n'
 }
