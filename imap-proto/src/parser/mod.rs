7a32686cb6dd1320dae41426894648fe3c33d8a5
Damian Poddebniak <poddebniak@fh-muenster.de>
2020-03-28 20:17:41 +0100

    Move ParseResult

--- imap-proto/src/parser/mod.rs
+++ imap-proto/src/parser/mod.rs
@@ -1,3 +1,6 @@
+use nom::IResult;
+use crate::types::Response;
+
 pub use self::rfc3501::*;
 pub use self::rfc5464::*;
 
@@ -6,3 +9,5 @@ pub mod core;
 mod rfc3501;
 mod rfc4551;
 mod rfc5464;
+
+pub type ParseResult<'a> = IResult<&'a [u8], Response<'a>>;
