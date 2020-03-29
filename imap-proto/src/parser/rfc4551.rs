aa9eef1d5a1ebd63601fd7a100670f474267a90a
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-03-29 14:57:34 +0200

    Convert RFC 4551 parsers to function style

--- imap-proto/src/parser/rfc4551.rs
+++ imap-proto/src/parser/rfc4551.rs
@@ -5,8 +5,7 @@
 //! or Quick Flag Changes Resynchronization
 //!
 
-// rustfmt doesn't do a very good job on nom parser invocations.
-#![cfg_attr(rustfmt, rustfmt_skip)]
+use nom::{bytes::streaming::tag_no_case, sequence::tuple, IResult};
 
 use crate::{parser::core::number_64, types::*};
 
@@ -14,24 +13,21 @@ use crate::{parser::core::number_64, types::*};
 // Extends resp-test-code defined in rfc3501.
 // [RFC4551 - 3.6 HIGHESTMODSEQ Status Data Items](https://tools.ietf.org/html/rfc4551#section-3.6)
 // [RFC4551 - 4. Formal Syntax - resp-text-code](https://tools.ietf.org/html/rfc4551#section-4)
-named!(pub (crate) resp_text_code_highest_mod_seq<ResponseCode>, do_parse!(
-    tag_no_case!("HIGHESTMODSEQ ") >>
-    num: number_64 >>
-    (ResponseCode::HighestModSeq(num))
-));
+pub(crate) fn resp_text_code_highest_mod_seq(i: &[u8]) -> IResult<&[u8], ResponseCode> {
+    let (i, (_, num)) = tuple((tag_no_case("HIGHESTMODSEQ "), number_64))(i)?;
+    Ok((i, ResponseCode::HighestModSeq(num)))
+}
 
 // Extends status-att/status-att-list defined in rfc3501
 // [RFC4551 - 3.6 - HIGHESTMODSEQ Status Data Items](https://tools.ietf.org/html/rfc4551#section-3.6)
 // [RFC4551 - 4. Formal Syntax - status-att-val](https://tools.ietf.org/html/rfc4551#section-4)
-named!(pub (crate) status_att_val_highest_mod_seq<StatusAttribute>, do_parse!(
-    tag_no_case!("HIGHESTMODSEQ ") >>
-    mod_sequence_valzer: number_64 >>
-    (StatusAttribute::HighestModSeq(mod_sequence_valzer))
-));
+pub(crate) fn status_att_val_highest_mod_seq(i: &[u8]) -> IResult<&[u8], StatusAttribute> {
+    let (i, (_, num)) = tuple((tag_no_case("HIGHESTMODSEQ "), number_64))(i)?;
+    Ok((i, StatusAttribute::HighestModSeq(num)))
+}
 
 // [RFC4551 - 4. Formal Syntax - fetch-mod-resp](https://tools.ietf.org/html/rfc4551#section-4)
-named!(pub (crate) msg_att_mod_seq<AttributeValue>, do_parse!(
-    tag_no_case!("MODSEQ ") >>
-    num: paren_delimited!(number_64) >>
-    (AttributeValue::ModSeq(num))
-));
+pub(crate) fn msg_att_mod_seq(i: &[u8]) -> IResult<&[u8], AttributeValue> {
+    let (i, (_, num)) = tuple((tag_no_case("MODSEQ "), number_64))(i)?;
+    Ok((i, AttributeValue::ModSeq(num)))
+}
