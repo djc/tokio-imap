f45110e1f897beab8be8dd43cfa9d2b19eacab65
Dirkjan Ochtman <dirkjan@ochtman.nl>
2020-05-06 22:15:34 +0200

    Update fuzz target code

--- imap-proto/fuzz/fuzz_targets/utf8_parse_response.rs
+++ imap-proto/fuzz/fuzz_targets/utf8_parse_response.rs
@@ -4,8 +4,5 @@ extern crate imap_proto;
 
 // UTF-8
 fuzz_target!(|data: &[u8]| {
-    use imap_proto::parse_response;
-    if let Ok(string_data) = std::str::from_utf8(data) {
-        let _ = parse_response(string_data.as_bytes());
-    }
+    let _ = imap_proto::Response::from_bytes(data);
 });
