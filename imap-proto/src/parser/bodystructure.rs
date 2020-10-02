d9aac992024949cee0f2e06e968497a640ca649c
Yannick Heinrich <yannick.heinrich@gmail.com>
2020-10-02 10:08:13 +0200

    Adding a `BodyStructParser`
    Regarding #92, I propose to add an helper parser to be able to search
    out specific component from a `BodyStructure`. This will allow for
    example to retrieve the body section ID of some attachment from the
    BODYSTRUCTURE and perform a FETCH request in a second time to download
    only this specific element.

---
+++ imap-proto/src/parser/bodystructure.rs
@@ -0,0 +1,76 @@
+use std::collections::HashMap;
+
+use crate::types::BodyStructure;
+/// An utility parser helping to find the appropriate
+/// section part from a FETCH response.
+pub struct BodyStructParser<'a> {
+    root: &'a BodyStructure<'a>,
+    prefix: Vec<u32>,
+    iter: u32,
+    map: HashMap<Vec<u32>, &'a BodyStructure<'a>>,
+}
+
+impl<'a> BodyStructParser<'a> {
+    /// Returns a new parser
+    ///
+    /// # Arguments
+    ///
+    /// * `root` - The root of the `BodyStructure response.
+    pub fn new(root: &'a BodyStructure<'a>) -> Self {
+        let mut parser = BodyStructParser {
+            root,
+            prefix: vec![],
+            iter: 1,
+            map: HashMap::new(),
+        };
+
+        parser.parse(parser.root);
+        parser
+    }
+
+    /// Search particular element within the bodystructure.
+    ///
+    /// # Arguments
+    ///
+    /// * `func` - The filter used to search elements within the bodystructure.
+    pub fn search<F>(&self, func: F) -> Option<Vec<u32>>
+    where
+        F: Fn(&'a BodyStructure<'a>) -> bool,
+    {
+        let elem: Vec<_> = self
+            .map
+            .iter()
+            .filter_map(|(k, v)| {
+                if func(*v) {
+                    let slice: &[u32] = k;
+                    Some(slice)
+                } else {
+                    None
+                }
+            })
+            .collect();
+        elem.first().map(|a| a.to_vec())
+    }
+
+    /// Reetr
+    fn parse(&mut self, node: &'a BodyStructure) {
+        match node {
+            BodyStructure::Multipart { bodies, .. } => {
+                let vec = self.prefix.clone();
+                self.map.insert(vec, node);
+
+                for (i, n) in bodies.iter().enumerate() {
+                    self.iter += i as u32;
+                    self.prefix.push(self.iter);
+                    self.parse(n);
+                    self.prefix.pop();
+                }
+                self.iter = 1;
+            }
+            _ => {
+                let vec = self.prefix.clone();
+                self.map.insert(vec, node);
+            }
+        };
+    }
+}
