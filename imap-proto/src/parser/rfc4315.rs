d4e247395c5323f990726748e045618a0bfc7d94
Dirkjan Ochtman <dirkjan@ochtman.nl>
2021-01-15 10:40:52 +0100

    Fix up doctest failures

--- imap-proto/src/parser/rfc4315.rs
+++ imap-proto/src/parser/rfc4315.rs
@@ -18,11 +18,13 @@ use crate::types::*;
 
 /// Extends resp-text-code as follows:
 ///
+/// ```ignore
 ///     resp-text-code =/ resp-code-apnd
 ///     resp-code-apnd = "APPENDUID" SP nz-number SP append-uid
 ///     append-uid      =/ uid-set
 ///                       ; only permitted if client uses [MULTIAPPEND]
 ///                       ; to append multiple messages.
+/// ```
 ///
 /// [RFC4315 - 3 Additional Response Codes](https://tools.ietf.org/html/rfc4315#section-3)
 pub(crate) fn resp_text_code_append_uid(i: &[u8]) -> IResult<&[u8], ResponseCode> {
@@ -37,8 +39,10 @@ pub(crate) fn resp_text_code_append_uid(i: &[u8]) -> IResult<&[u8], ResponseCode
 
 /// Extends resp-text-code as follows:
 ///
+/// ```ignore
 ///     resp-text-code =/ resp-code-copy
 ///     resp-code-copy = "COPYUID" SP nz-number SP uid-set
+/// ```
 ///
 /// [RFC4315 - 3 Additional Response Codes](https://tools.ietf.org/html/rfc4315#section-3)
 pub(crate) fn resp_text_code_copy_uid(i: &[u8]) -> IResult<&[u8], ResponseCode> {
@@ -53,7 +57,9 @@ pub(crate) fn resp_text_code_copy_uid(i: &[u8]) -> IResult<&[u8], ResponseCode> 
 
 /// Extends resp-text-code as follows:
 ///
+/// ```ignore
 ///     resp-text-code =/ "UIDNOTSTICKY"
+/// ```
 ///
 /// [RFC4315 - 3 Additional Response Codes](https://tools.ietf.org/html/rfc4315#section-3)
 pub(crate) fn resp_text_code_uid_not_sticky(i: &[u8]) -> IResult<&[u8], ResponseCode> {
@@ -62,7 +68,9 @@ pub(crate) fn resp_text_code_uid_not_sticky(i: &[u8]) -> IResult<&[u8], Response
 
 /// Parses the uid-set nonterminal:
 ///
+/// ```ignore
 ///     uid-set = (uniqueid / uid-range) *("," uid-set)
+/// ```
 ///
 /// [RFC4315 - 4 Formal Syntax](https://tools.ietf.org/html/rfc4315#section-4)
 fn uid_set(i: &[u8]) -> IResult<&[u8], Vec<UidSetMember>> {
@@ -71,10 +79,12 @@ fn uid_set(i: &[u8]) -> IResult<&[u8], Vec<UidSetMember>> {
 
 /// Parses the uid-set nonterminal:
 ///
+/// ```ignore
 ///    uid-range = (uniqueid ":" uniqueid)
 ///                ; two uniqueid values and all values
 ///                ; between these two regards of order.
 ///                ; Example: 2:4 and 4:2 are equivalent.
+/// ```
 ///
 /// [RFC4315 - 4 Formal Syntax](https://tools.ietf.org/html/rfc4315#section-4)
 fn uid_range(i: &[u8]) -> IResult<&[u8], UidSetMember> {
