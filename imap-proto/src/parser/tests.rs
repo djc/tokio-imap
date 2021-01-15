817a88371b2c1a0b42b95b029cab45218645d81b
Milo Mirate <mmirate@users.noreply.github.com>
2021-01-15 12:27:24 -0500

    try again

--- imap-proto/src/parser/tests.rs
+++ imap-proto/src/parser/tests.rs
@@ -439,7 +439,6 @@ fn test_uidplus() {
 
     // TODO this panics:
     // assert!(dbg!(parse_response(b"* OK [COPYUID 38505 304, 319:320 3956:3958] Done\r\n")).is_err())
-    //
     // [imap-proto\src\parser\tests.rs:440] parse_response(b"* OK [COPYUID 38505 304, 319:320 3956:3958] Done\r\n") = Ok(
     //     (
     //         [],
